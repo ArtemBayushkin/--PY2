1b30116db5857a6d141389aaff4eae686c3a5a8e
ArtemBayushkin <115110907+ArtemBayushkin@users.noreply.github.com>
2023-01-14 17:26:41 +0300

    Update class_Library.py

--- Лабораторная работа 2/class_Library.py
+++ Лабораторная работа 2/class_Library.py
@@ -31,11 +31,12 @@ class Library:
         return len(self.books) + 1
 
     def get_index_by_book_id(self, id_):
-        for i, id_ in enumerate(self.books):
-            if id_ in self.books:  # Не понял, как выделить id из листа с данными о книгах. Тут явная ошибка!
-                return i
-        #if index_ not in self.books:
-        #    ValueError("Книги с запрашиваемым id не существует")
+        flag = False
+        for index, value in enumerate(self.books):
+            if value.id_ == id_:
+                return index
+        if flag is False:
+            raise ValueError("Книги с запрашиваемым id не существует")
 
 
 if __name__ == '__main__':
@@ -48,4 +49,4 @@ if __name__ == '__main__':
     library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
     print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
 
-    print(library_with_books.get_index_by_book_id(2))  # проверяем индекс книги с id = 1
+    print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
