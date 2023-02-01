1e91c1e6aa867b2e18c9beea440acfc5e953cc79
ArtemBayushkin <115110907+ArtemBayushkin@users.noreply.github.com>
2023-02-01 18:35:13 +0300

    Update class_Book.py

--- Лабораторная работа 2/class_Book.py
+++ Лабораторная работа 2/class_Book.py
@@ -11,27 +11,53 @@ BOOKS_DATABASE = [
     }
 ]
 
+
 class Book:
-    def __init__(self, id_, name:str, pages):
-        self.name=name
-        self.id_=id_
-        self.pages=pages
+    def __init__(self, id_, name: str, pages):
+        """
+        Создание и подготовка к работе объекта "Книга"
+        :param id_: ID книги
+        :param name: Название книги
+        :param pages: Кол-во страниц книги
+        """
+        self.name = name
+        self.id_ = id_
+        self.pages = pages
 
-    def __str__(self) -> str:
-        self.name
-        return f'Книга "{self.name}"'
 
-    def __repr__(self):
-        return f'Book(id_={self.id_}, name={self.name!r}, pages={self.pages})'
+class Library:
+    def __init__(self, books: list[Book] = None):
+        """
+        Создание и подготовка к работе объекта "Библиотека"
+        :param books: список книг из объекта "Книга"
+        """
+        if books is None:                           # Проверка. Если переменная пуста, как изначально записано
+            books = []                              # переменная принимает пустой список
+        self.books = books                          # Описание объекта
 
+    def get_next_book_id(self):                     # Метод, возвращающий ID для добавления новой книги
+        return len(self.books) + 1                  # Возвращает увеличенный на 1 ID для добавления новой книги
+    """ С точки зрения быстродействия такой метод менее эффективен, нежели использовать обратную индексацию списка. 
+    Если будет миллион книг, то методу придется считать длину достаточно долго. Однако, если книг совсем нет, то
+    индексация работать не будет, так как нет листа"""
+
+    def get_index_by_book_id(self, id_):            # Метод, возвращающий ID книги
+        flag = False
+        for index, value in enumerate(self.books):  # Перебор ID в списке книг
+            if value.id_ == id_:                    # Если во время перебора был найден ID
+                return index                        # ID возвращается
+        if flag is False:                           # Если ID не найден, возвращается ошибка
+            raise ValueError("Книги с запрашиваемым id не существует")
 
 
 if __name__ == '__main__':
-    # инициализируем список книг
+    empty_library = Library()  # инициализируем пустую библиотеку
+    print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
+
     list_books = [
         Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
     ]
-    for book in list_books:
-        print(book)  # проверяем метод __str__
+    library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
+    print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
 
-    print(list_books)  # проверяем метод __repr__
+    print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
